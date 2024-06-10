267f0ef1f34af3f78fb2113f8ac1ad8ad4855bd1
donghyuun <bomul0524@gmail.com>
2024-06-10 19:40:48 +0900

    feat: set up project

---
+++ src/main/java/com/example/apapbackend/ApapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.apapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApapBackendApplication.class, args);
+	}
+
+}
