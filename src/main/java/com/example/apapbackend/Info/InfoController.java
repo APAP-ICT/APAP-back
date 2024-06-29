df4845bd4015ceee7e8b1238bb0a2aee40dd2b4b
donghyuun <bomul0524@gmail.com>
2024-06-29 15:41:06 +0900

    feat: 모든 객체 탐지 정보 조회 & API URL 수정

--- src/main/java/com/example/apapbackend/Info/InfoController.java
+++ src/main/java/com/example/apapbackend/Info/InfoController.java
@@ -1,13 +1,13 @@
 package com.example.apapbackend.Info;
 
 import com.example.apapbackend.S3.S3ImageFileUploader;
-import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +19,35 @@ public class InfoController {
 
     private final S3ImageFileUploader s3ImageFileUploader;
     private final InfoService infoService;
-    @PostMapping("/info")
-    public ResponseEntity<Info> uploadFile(
+
+    /**
+     * 객체 탐지 정보 저장
+     *
+     * @param localDateTime
+     * @param label
+     * @param base64Image
+     * @return
+     */
+    @PostMapping("/infos")
+    public ResponseEntity<Info> postInfo(
         @RequestParam("datetime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime localDateTime,
         @RequestParam("label") String label,
         @RequestParam("image") String base64Image
     ) {
         log.info("/file POST 요청");
         ResponseEntity<Info> response = infoService.save(localDateTime, label, base64Image);
+        return response;
+    }
+
+    /**
+     * 모든 객체 탐지 정보 조회
+     *
+     * @return
+     */
+    @GetMapping("/infos")
+    public ResponseEntity<List<Info>> getInfos() {
+        log.info("/infos GET 요청");
+        ResponseEntity<List<Info>> response = infoService.getInfos();
         return response;
     }
 }
--- src/main/java/com/example/apapbackend/Info/InfoService.java
+++ src/main/java/com/example/apapbackend/Info/InfoService.java
@@ -1,8 +1,11 @@
 package com.example.apapbackend.Info;
 
+import com.amazonaws.Response;
 import com.example.apapbackend.S3.S3ImageFileUploader;
+import jakarta.persistence.criteria.CriteriaBuilder.In;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +18,16 @@ public class InfoService {
     private final S3ImageFileUploader s3ImageFileUploader;
     private final InfoRepository infoRepository;
 
-    public ResponseEntity<Info> save(LocalDateTime localDateTime, String label, String base64Image) {
+    /**
+     * 객체 정보 저장
+     *
+     * @param localDateTime
+     * @param label
+     * @param base64Image
+     * @return DB 에 저장한 객체 정보 반환
+     */
+    public ResponseEntity<Info> save(LocalDateTime localDateTime, String label,
+        String base64Image) {
         try {
             String objUrl = s3ImageFileUploader.uploadImageFromUrlToS3(base64Image);
             Info info = new Info(localDateTime, label, objUrl);
@@ -26,5 +38,15 @@ public class InfoService {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    /**
+     * 모든 객체 탐지 정보 조회
+     *
+     * @return
+     */
+    public ResponseEntity<List<Info>> getInfos() {
+        List<Info> infos = infoRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(infos);
     }
 }
