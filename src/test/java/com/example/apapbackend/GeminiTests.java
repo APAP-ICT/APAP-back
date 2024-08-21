cd855a206696739d1d8273a1596660da06d22139
donghyuun <bomul0524@gmail.com>
2024-08-21 14:49:13 +0900

    test: s3 객체 주소를 통해 gemini 설명 추출

--- src/test/java/com/example/apapbackend/GeminiTests.java
+++ src/test/java/com/example/apapbackend/GeminiTests.java
@@ -1,7 +1,13 @@
 package com.example.apapbackend;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.amazonaws.services.s3.AmazonS3;
 import com.example.apapbackend.global.gemini.GeminiRequest;
 import com.example.apapbackend.global.gemini.GeminiService;
+import com.example.apapbackend.global.s3.S3Service;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Base64;
@@ -14,6 +20,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class GeminiTests {
     @Autowired
     private GeminiService service;
+    @Autowired
+    private AmazonS3 s3Client;
+    @Autowired
+    private S3Service s3Service;
 
     @Test
     @Description("텍스트만 사용해 요청")
@@ -37,4 +47,13 @@ public class GeminiTests {
         );
         System.out.println(text);
     }
+
+    @Test
+    public void testGetDescription() throws IOException {
+        // 준비 단계: S3 객체 Mock 설정
+        String s3ObjUrl = "https://apap-back.s3.ap-northeast-2.amazonaws.com/01763b79-9c8b-49a3-8bdc-474dbca72333";
+        String description = service.getDescription(s3ObjUrl);
+        System.out.println("description = " + description);
+    }
+
 }
